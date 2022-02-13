767781e6e98958e0e09faa1a0784615c89b5b974
Nick Askam <asknickfilms@gmail.com>
2022-02-13 17:17:58 -0600

    adding in the teams

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,14 @@
 import React from "react"
 
 function HomePage({ name }) {
+    
     let selectedTeams = 0;
+
+    const handleChange = () => {
+      selectedTeams ++;
+      console.log("selected teams: " + selectedTeams);
+    };
+    // change to make it change based on 
 
     return (
         <>
@@ -17,24 +24,51 @@ function HomePage({ name }) {
             <div className="teamMatrix">
                 <div className="row">
                     <p className="team">Division:</p>
-                    <p className="team">Team 1:</p>
-                    <p className="team">Team 2:</p>
-                    <p className="team">Team 3:</p>
-                    <p className="team">Team 4:</p>
+                    <p></p>
+                    <p></p>
+                    <p>Division Filter:</p>
+                    <p>
+                        <select>
+                            <option value="all">All</option>
+                            <option value="nfc-east">NFC East</option>
+                            <option value="nfc-north">NFC North</option>
+                            <option value="nfc-south">NFC South</option>
+                            <option value="nfc-west">NFC West</option>
+                        </select>
+                    </p>
                 </div>
                 <div className="row">
-                    <p className="team">NFC East:</p>
-                    <p className="team">Dallas Cowboys</p>
-                    <p className="team">Philadelphia Eagles</p>
-                    <p className="team">New York Giants</p>
-                    <p className="team">Washington Commanders</p>
+                    <p className="team n-e">NFC East:</p>
+                    <p className="team n-e">
+                        <input
+                        type="checkbox"
+                        onChange={handleChange} />
+                        Dallas Cowboys
+                    </p>
+                    <p className="team n-e">Philadelphia Eagles</p>
+                    <p className="team n-e">New York Giants</p>
+                    <p className="team n-e">Washington Commanders</p>
                 </div>
                 <div className="row">
-                    <p className="team">NFC North:</p>
-                    <p className="team">Green Bay Packers</p>
-                    <p className="team">Minnesota Vikings</p>
-                    <p className="team">Chicago Bears</p>
-                    <p className="team">Detroit Lions</p>
+                    <p className="team n-n">NFC North:</p>
+                    <p className="team n-n">Green Bay Packers</p>
+                    <p className="team n-n">Minnesota Vikings</p>
+                    <p className="team n-n">Chicago Bears</p>
+                    <p className="team n-n">Detroit Lions</p>
+                </div>
+                <div className="row">
+                    <p className="team n-s">NFC South:</p>
+                    <p className="team n-s">Tampa Bay Buccanneers</p>
+                    <p className="team n-s">New Orleans Saints</p>
+                    <p className="team n-s">Atlanta Falcons</p>
+                    <p className="team n-s">Carolina Panthers</p>
+                </div>
+                <div className="row">
+                    <p className="team n-w">NFC West:</p>
+                    <p className="team n-w">Los Angeles Rams</p>
+                    <p className="team n-w">San Francisco 49ers</p>
+                    <p className="team n-w">Arizona Cardinals</p>
+                    <p className="team n-w">Seattle Seahawks</p>
                 </div>
             </div>
 
