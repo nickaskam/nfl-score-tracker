430da775a7b0fcf56a82e41c6a329eb115bbd210
Nick Askam <asknickfilms@gmail.com>
2022-02-27 18:13:24 -0600

    adding folder for front end in preparation for backend

--- frontend/src/pages/OneGameMatchup.js
+++ frontend/src/pages/OneGameMatchup.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React from "react"; // , {useState, useEffect} 
 import { useParams } from "react-router-dom";
 import stadiumLogo from "../assets/lumenField.png";
-import cowboysLogo from "../assets/cowboys.png";
-import seahawksLogo from "../assets/seahawks.png";
+// import cowboysLogo from "../assets/cowboys.png";
+// import seahawksLogo from "../assets/seahawks.png";
 import nflTeams from "../data/nflTeams.json";
 import nflGames3 from "../data/gameScores3.json";
+// import socketIOClient from "socket.io-client";
+// const ENDPOINT = "http://192.168.86.24:5050";
 
 function OneGameMatchup() {
     let { gameid } = useParams()
@@ -35,6 +37,43 @@ function OneGameMatchup() {
     }
     console.log(team1Object)
     console.log(team2Object)
+
+    // var token_ // variable will store the token
+    // var userName = "admin_T42"; // app clientID
+    // var passWord = "admin_T42"; // app clientSecret
+    // var caspioTokenUrl = "https://us-central1-osu-project-342203.cloudfunctions.net/image-generator?query=atlanta+falcons+logo+nfl&size=small"; // Your application token endpoint  
+    // var request = new XMLHttpRequest(); 
+
+    // function getToken(url, clientID, clientSecret) {
+    //     var key;           
+    //     request.open("POST", url, true); 
+    //     request.setRequestHeader("Content-type", "application/json");
+    //     request.send("grant_type=client_credentials&client_id="+clientID+"&"+"client_secret="+clientSecret); // specify the credentials to receive the token on request
+    //     request.onreadystatechange = function () {
+    //         if (request.readyState == request.DONE) {
+    //             var response = request.responseText;
+    //             var obj = JSON.parse(response); 
+    //             key = obj.access_token; //store the value of the accesstoken
+    //             token_ = key; // store token in your global variable "token_" or you could simply return the value of the access token from the function
+    //         }
+    //     }
+    // }
+    // // Get the token
+    // console.log(getToken(caspioTokenUrl, userName, passWord))
+
+    // const [response, setResponse] = useState("");
+    // let data = '';
+
+    // useEffect(() => {
+    //   const socket = socketIOClient(ENDPOINT);
+    //   socket.on("hello", data => {
+    //     setResponse(data);
+    //   });
+
+    //   return () => socket.disconnect();
+    // }, []);
+
+    // console.log(data)
 
     return (
         <>
@@ -71,7 +110,7 @@ function OneGameMatchup() {
                 </table>
             </div>
             <div>
-                <p>What happened on this day: </p>
+                <p>More About the Home Team - {team1Object.full_name}: </p>
                 <ul>
                     <li>place holder</li>
                     <li>place holder</li>
